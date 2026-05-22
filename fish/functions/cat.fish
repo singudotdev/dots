55899fa52664d8df42f9d70e4db7f726808a4da9
singudotdev <contact@singu.dev>
2026-05-22 16:47:53 +0200

    alias(fish): add cat/bat functions

---
+++ fish/functions/cat.fish
@@ -0,0 +1,3 @@
+function cat --wraps='bat' --description 'alias cat=bat (full features)'
+    bat $argv
+end
---
+++ fish/functions/lcat.fish
@@ -0,0 +1,3 @@
+function lcat --wraps='/bin/cat' --description 'alias lcat=/bin/cat (legacy)'
+    /bin/cat $argv
+end
